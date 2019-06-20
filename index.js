aa28e809576e5ed0a53f0a995456a40c06b562a1
papico <ocipap0531@gmail.com>
2019-06-20 19:29:22 +0900

    feat : level1 기본설정

---
+++ index.js
@@ -0,0 +1,20 @@
+function curry() {}
+
+function map() {}
+
+function filter() {}
+
+function reduce () {}
+
+function pipe() {}
+
+function go() {}
+
+export {
+  curry,
+  map,
+  filter,
+  reduce,
+  pipe,
+  go
+}
