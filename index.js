069915220b7d68d54d1600ba9dd0f34c3fad70ef
papico <ocipap0531@gmail.com>
2019-06-28 20:44:35 +0900

    feat : level2 기본설정

---
+++ index.js
@@ -0,0 +1,41 @@
+import {
+  curry,
+  map,
+  filter,
+  reduce,
+  pipe,
+  go
+} from './util'
+
+const pluck = () => {
+
+}
+
+const reject = () => {
+
+}
+
+const compact = () => {
+
+}
+
+const find = () => {
+
+}
+
+const some = () => {
+
+}
+
+const every = () => {
+
+}
+
+export {
+  pluck,
+  reject,
+  compact,
+  find,
+  some,
+  every
+}
